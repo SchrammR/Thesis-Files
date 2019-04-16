ffed07962fccb8693b1a1d42967dfc56fa1aa59a
Robin Schramm <robinschramm@gmx.de>
2019-04-16 13:13:30 +0200

    added Head collider
    hazards can now move

--- FinalIk-VR/Assets/_Scripts/DodgeGameController.cs
+++ FinalIk-VR/Assets/_Scripts/DodgeGameController.cs
@@ -8,14 +8,15 @@ public class DodgeGameController : MonoBehaviour
 {
     public int score = 0;
     public Text pointsText;
-    public MeshCollider playerCollider;
+    public SphereCollider playerCollider;
     public BoxCollider hazardCollider;
     public bool gameIsStarted = false;
     public GameObject hazard;
     public float roomSizeX, roomSizeY, roomSizeZ;
+    private GameObject hazardInstance;
+
 
     public int hazardCount = 0;
-
     void Start()
     {
         pointsText.text = "Punkte: " + score;
@@ -23,23 +24,30 @@ public class DodgeGameController : MonoBehaviour
 
     void Update()
     {
-        if( gameIsStarted){
+        if(gameIsStarted)
+        {
             pointsText.text = "Punkte: " + score;
             //in intervall random
-            if(hazardCount < 3){
-                createHazards(new Vector3(Random.Range(roomSizeX, -roomSizeX), Random.Range(0, roomSizeY/2), 0), new Vector3(1, 1, 16));
-            }
+            createHazards(new Vector3(Random.Range(roomSizeX, -roomSizeX), Random.Range(0, roomSizeY/2), 0), new Vector3(1, 1, 16));
         }
     }
 
     private void createHazards(Vector3 coordinates, Vector3 size){
-        GameObject hazardInstance;
-        hazardInstance = Instantiate(hazard, coordinates, Quaternion.identity);
-        hazardInstance.transform.localScale = size;
-        hazardCount++;
+        spawnBoxHazard();
     }
 
-
+    private void spawnBoxHazard()
+    {
+        if (hazardCount < 1)
+        {
+            hazardInstance = Instantiate(hazard, new Vector3(5, 2, 0), Quaternion.identity);
+            hazardInstance.transform.localScale = new Vector3(1, 1, 6);
+            hazardCount++;
+        }
+        // Move our position a step closer to the target.
+        float step = 1 * Time.deltaTime; // calculate distance to move
+        hazardInstance.transform.position = Vector3.MoveTowards(hazardInstance.transform.position, new Vector3(-5, 2, 0), step);
+    }
 
 
 
