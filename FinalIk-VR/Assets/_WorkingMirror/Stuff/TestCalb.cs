cb9cdecbf6bd0a76c8c28122927ee0291f87df6e
Ravell Heerdegen <ravellheerdegen@outlook.com>
2019-05-04 12:20:24 +0200

    mirror works

---
+++ FinalIk-VR/Assets/_WorkingMirror/Stuff/TestCalb.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestCalb : MonoBehaviour
+{
+    public Transform FaceTarget;
+    public Transform HMD;
+
+    public Transform Avatar;
+
+    private float heightEyeOne = 1.96f;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            Calibrate();
+        }
+    }
+
+    void Calibrate()
+    {
+        float scalar =  HMD.position.y/heightEyeOne;
+
+        Avatar.localScale =Vector3.one*  scalar;
+    }
+}
